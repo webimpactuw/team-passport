9111bee8ac264319dbc691a2d99f533106c539a5
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2024-04-20 15:07:05 -0700

    add pic

--- app/src/pages/QuizzesPage.js
+++ app/src/pages/QuizzesPage.js
@@ -1,11 +1,22 @@
+import { CenterContainer } from "../components/Container"
+import { Header } from "../components/Header"
 import { QuizBoard } from "../components/Quizboard"
+import logo from "../math.jpg"
 
 export default function QuizzesPage() {
     return (
-        <QuizBoard question={`Which of the following is divisible by 4?`}
-            option1={`1`}
-            option2={`12`}
-            option3={`123`}
-            option4={`1234`} />
+        <CenterContainer>
+            <Header>
+                Quiz Progress
+            </Header>
+            <CenterContainer style={`grid-cols-2 w-3/5 h-3/5 m-20 gap-10`}>
+                <img src={logo} alt="Logo"/>
+                <QuizBoard question={`Which of the following is divisible by 4?`}
+                    option1={`1`}
+                    option2={`12`}
+                    option3={`123`}
+                    option4={`1234`} />
+            </CenterContainer>
+        </CenterContainer>
     )
 }
