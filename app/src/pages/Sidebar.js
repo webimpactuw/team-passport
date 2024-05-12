b694b0da3a523e8b0198fca40fe8d5f61791a10e
leowjli <leo.wj.li@gmail.com>
2024-05-11 20:19:04 -0700

    sidebar styling changes

--- app/src/pages/Sidebar.js
+++ app/src/pages/Sidebar.js
@@ -13,10 +13,10 @@ export default function Sidebar() {
       <div className="sidebar fixed top-8 left-6">
         <button type="button" className="btn" onClick={toggleSideBar}><IconButton link="" icon={faBars}></IconButton></button>
         <ul className={openMenu ? "block" : "hidden"}>
-          <li className="flex items-center group"><IconButton link="/" icon={faGrip}/><p className="ml-2 opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Dashboard</p></li>
-          <li className="flex items-center group"><IconButton link="/quiz" icon={faClipboardQuestion}/><p className="ml-2 opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Quiz</p></li>
-          <li className="flex items-center group"><IconButton link="/profile" icon={faUser}/><p className="ml-2 opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Profile</p></li>
-          <li className="flex items-center group"><IconButton link="/logout" icon={faArrowRightFromBracket}/><p className="ml-2 opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Log Out</p></li>
+          <li className="flex items-center group"><IconButton link="/" icon={faGrip}/><p className="p-3.5 bg-cyan-300 rounded-xl opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Dashboard</p></li>
+          <li className="flex items-center group"><IconButton link="/quiz" icon={faClipboardQuestion}/><p className="p-3.5 bg-cyan-300 rounded-xl opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Quiz</p></li>
+          <li className="flex items-center group"><IconButton link="/profile" icon={faUser}/><p className="p-3.5 bg-cyan-300 rounded-xl opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Profile</p></li>
+          <li className="flex items-center group"><IconButton link="/logout" icon={faArrowRightFromBracket}/><p className="p-3.5 bg-cyan-300 rounded-xl opacity-0 pointer-event-none group-hover:opacity-100 group-hover:pointer-events-auto">Log Out</p></li>
         </ul>
       </div>
   )
